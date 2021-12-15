c99192cd6f0050c85bd8746b6c58fd1ad1755550
Jackson Cole <jacksoncole@Jacksons-MacBook-Pro.local>
2021-12-15 14:54:21 -0800

    Connect to Database

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv').config();
+var database = require('./config/database');
 
 var indexRouter = require('./routes/index');
 var songsRouter = require('./routes/songs');
